b54d783972a8c4b4a3b8335e6bc264f6268d914f
Brogan Duffy <broganduffy859@gmail.com>
2017-08-15 12:57:43 +0400

    adds

---
+++ coffee/js/menu.js
@@ -0,0 +1,15 @@
+/**
+ * Created by us on 6/13/2017.
+ */
+
+$( ".menu-icon" ).click(function() {
+    $( ".menu-hide" ).slideToggle( "slow", function() {
+        // Animation complete.
+    });
+});
+
+// $( "header .content .header-menu ul li a" ).click(function() {
+//     $( ".mainmenu-sub" ).slideToggle( "slow", function() {
+//         // Animation complete.
+//     });
+// });
